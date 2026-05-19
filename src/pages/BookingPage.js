6c97f45525bbaeea7abd1cd88f6a30a1f635c4ce
Fadi <fadibatto7@gmail.com>
2026-05-19 19:54:23 +0300

    update booking page

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -641,7 +641,7 @@ export default function BookingPage() {
             onClick={() => {
 
               if (guests < 8) {
-                setGuests(guests -1);
+                setGuests(guests +1);
               }
 
             }}
